945359fab6d49c2c4b11c053b936213a3a290908
Jacob Cambell <jacob@jacobcambell.com>
2021-11-29 19:49:50 -0700

    Set up express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,12 @@
-console.log('it works')
+require('dotenv').config()
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 9000;
+
+app.post('/submit-form', (req, res) => {
+
+})
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+})
