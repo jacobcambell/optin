799ca5db1047f06dfda80c469b0b7eeae3ce7d36
Jacob Cambell <jacob@jacobcambell.com>
2021-12-01 10:43:35 -0700

    Wrap app in react-query provider

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,1 +1,13 @@
+import React from 'react'
 import './src/styles/global.css';
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {element}
+        </QueryClientProvider>
+    )
+}
