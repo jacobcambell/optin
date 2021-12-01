e6e4a3652c9c50184b4db4675cee65e53079237a
Jacob Cambell <jacob@jacobcambell.com>
2021-12-01 10:43:46 -0700

    Send form data to API

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,33 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from 'react-query'
+import axios from 'axios'
 
 const fieldStyle = 'border border-gray-500 w-full p-1 mb-3';
 const labelStyle = 'font-bold';
 
+interface FormFields {
+    email: string,
+    first_name: string,
+    last_name: string,
+    phone: string,
+    address: string,
+    zip_code: string,
+    country: string
+}
+
 export default function Form() {
+
+    const { data, error, isLoading, mutate } = useMutation((data: FormFields) => {
+        return axios.post(process.env.GATSBY_API_URL + '/submit-form', data)
+    }, {
+        onError: (err) => {
+            console.log('Error!!! ' + err)
+        },
+        onSuccess: (data) => {
+            console.log(data)
+        }
+    })
+
+    const handleSubmit = () => {
+        mutate({
+            email,
+            first_name,
+            last_name,
+            phone,
+            address,
+            zip_code,
+            country
+        })
+    }
+
+    const [email, setEmail] = useState<string>('')
+    const [first_name, setFirstName] = useState<string>('')
+    const [last_name, setLastName] = useState<string>('')
+    const [phone, setPhone] = useState<string>('')
+    const [address, setAddress] = useState<string>('')
+    const [zip_code, setZipCode] = useState<string>('')
+    const [country, setCountry] = useState<string>('')
+
     return (
         <div className="w-1/3 m-auto my-5">
             <p className={labelStyle}>Email</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setEmail(e.target.value) }} />
 
             <p className={labelStyle}>First Name</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setFirstName(e.target.value) }} />
 
             <p className={labelStyle}>Last Name</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setLastName(e.target.value) }} />
 
             <p className={labelStyle}>Phone</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setPhone(e.target.value) }} />
 
             <p className={labelStyle}>Address</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setAddress(e.target.value) }} />
 
             <p className={labelStyle}>Zip Code</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setZipCode(e.target.value) }} />
 
             <p className={labelStyle}>Country</p>
-            <input type="text" className={fieldStyle} />
+            <input type="text" className={fieldStyle} onChange={e => { setCountry(e.target.value) }} />
 
-            <button className="bg-blue-600 text-white block w-full py-2">Submit</button>
+            <button className="bg-blue-600 text-white block w-full py-2" onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
